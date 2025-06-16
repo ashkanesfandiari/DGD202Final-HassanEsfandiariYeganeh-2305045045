3edcb1f61950efc43cd4db8b1d521e7b282586a0
ashkan esfandiari <163771201+ashkanesfandiari@users.noreply.github.com>
2025-06-16 22:10:05 +0500

    submission

--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,10 +9,31 @@ public class PlayerMover : MonoBehaviour
     [Header("Movement Parameters")]
     [field: SerializeField] public float MoveSpeed { get; private set; }
     [SerializeField] private float _turnSpeed;
+    private PlayerControls _playerControls;
+    
     
     private void Awake()
     {
+        _playerControls = new PlayerControls();
         _rigidbody = GetComponent<Rigidbody>();
+    }
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerControls.Disable();
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = _playerControls.player.move.ReadValue<Vector2>();
+        if (moveInput != Vector2.zero)
+        {
+            Move(moveInput);
+        }
     }
 
     public void Move(Vector2 direction)
